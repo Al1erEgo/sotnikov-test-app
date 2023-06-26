c7086e27b56e61dd8116c2a6b807c54157c7939c
Al1erEgo <al1erego@gmail.com>
2023-06-26 14:54:25 +0200

    feat: create GlobalStyle fix: downgrade version of styled components and install types

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./app/App"
+import { GlobalStyle } from "./common/styles"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <App />
+    <GlobalStyle />
   </Provider>,
 )
