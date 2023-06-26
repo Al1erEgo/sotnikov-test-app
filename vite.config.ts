826f527ebc9c93e3969a2484b829226cb7df3a62
Al1erEgo <al1erego@gmail.com>
2023-06-26 11:44:34 +0200

    feat: configure gh-actions to automatically deploy on push in main

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from "vitest/config"
+import {defineConfig} from "vitest/config"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/sotnikov-test-app/",
   plugins: [react()],
   server: {
     open: true,
