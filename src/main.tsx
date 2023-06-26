83ef2df99f19f95a4d5174a5b5d2b8d35c428dde
Al1erEgo <al1erego@gmail.com>
2023-06-26 17:26:11 +0200

    fix: App import

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import { App } from "./app/App"
 import { GlobalStyle } from "./common/styles"
 import { HashRouter } from "react-router-dom"
+import { App } from "./app/App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
