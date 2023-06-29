0c63b3a5af95a8a74cc22d55e42210c1ca5aed74
Al1erEgo <al1erego@gmail.com>
2023-06-29 17:42:49 +0200

    fix: add TODO

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,8 @@ import {AppRouter, ErrorPopup, TabsMenu} from "../common"
 
 //TODO убрать селекторы в отдельные файлы
 //TODO убрать санки в отдельные файлы
+//TODO починить иконку
+
 export const App = () => {
   return (
     <>
