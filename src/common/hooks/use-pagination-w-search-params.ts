12fa74e16a55b58181745613a68f832e45d5a20f
Al1erEgo <al1erego@gmail.com>
2023-07-01 00:49:43 +0200

    fix: pagination takes values from url instantly and correct

--- src/common/hooks/use-pagination-w-search-params.ts
+++ src/common/hooks/use-pagination-w-search-params.ts
@@ -1,8 +1,13 @@
-import { useEffect, useState } from "react"
-import { useSearchParams } from "react-router-dom"
+import {useEffect, useState} from "react"
+import {useSearchParams} from "react-router-dom"
 
+//TODO добавить проверку - если в url введены недопустимые значения, выставить дефолт
 export const usePaginationWSearchParams = <T>(data: T[]) => {
   const [searchParams, setSearchParams] = useSearchParams()
+
+  console.log("currentPage", searchParams.get("currentPage"))
+  console.log("pageSize", searchParams.get("pageSize"))
+
   const [paginationConfig, setPaginationConfig] = useState<[number, number]>([
     Number(searchParams.get("currentPage")) || 1,
     Number(searchParams.get("pageSize")) || 10,
@@ -22,7 +27,14 @@ export const usePaginationWSearchParams = <T>(data: T[]) => {
       currentPage: paginationConfig[0].toString(),
       pageSize: paginationConfig[1].toString(),
     })
-  }, [paginationConfig, setSearchParams])
+  }, [paginationConfig])
+
+  useEffect(() => {
+    setPaginationConfig([
+      Number(searchParams.get("currentPage")) || 1,
+      Number(searchParams.get("pageSize")) || 10,
+    ])
+  }, [searchParams])
 
   return { currentPageContent, paginationConfig, handlePaginationChange }
 }
