af8f6a913d0e07a6737038ffc0e53e17b1a03c90
Al1erEgo <al1erego@gmail.com>
2023-06-30 15:03:27 +0200

    fix: layout st2

--- src/modules/photos/pages/albums/styles.ts
+++ src/modules/photos/pages/albums/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components"
 
 export const AlbumsContainer = styled.div`
   max-height: calc(100% - 50px);
-  //height: calc(100% - 160px);
   min-height: 200px;
   overflow: auto;
   position: relative;
@@ -12,12 +11,4 @@ export const AlbumsContainer = styled.div`
   flex-wrap: wrap;
   gap: 10px;
   justify-content: center;
-
-  //display: grid;
-  //grid-template-columns: auto auto auto;
-  //gap: 10px;
-  //
-  //@media screen and (max-width: 500px) {
-  //  grid-template-columns: auto;
-  //}
 `
