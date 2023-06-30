574b00b4e9cff99e8a294372ba698e2239dfc48e
Al1erEgo <al1erego@gmail.com>
2023-06-30 22:02:22 +0200

    fix: album card height on edit

--- src/modules/photos/components/album-card/styles.ts
+++ src/modules/photos/components/album-card/styles.ts
@@ -5,8 +5,8 @@ import { NavLink } from "react-router-dom"
 export const StyledAlbumCard = styled(Card)<{ favourite: string }>`
   max-width: 300px;
   width: 300px;
-  max-height: 130px;
-  height: 130px;
+  max-height: fit-content;
+  min-height: 130px;
   border-color: ${(props) => (props.favourite ? "#1677ff" : "#e7e7e7")};
 `
 export const StyledPhotoCard = styled(Card)`
