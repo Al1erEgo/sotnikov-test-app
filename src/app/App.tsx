dab6889fe50e655d8c616a718ab57b4caa20bf46
Al1erEgo <al1erego@gmail.com>
2023-07-03 00:55:15 +0200

    fix: move providers to main.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,24 +1,14 @@
-import { Provider } from 'react-redux'
-import { HashRouter } from 'react-router-dom'
-import { PersistGate } from 'redux-persist/integration/react'
-
-import { AppRouter, ErrorPopup, GlobalStyle, StyledLoader, TabsMenu } from '../common'
-
-import { persistor, store } from './store'
+import { AppRouter, ErrorPopup, GlobalStyle, TabsMenu } from '../common'
 
 //TODO вынести комментарии в отдельный слайс в posts
 
 export const App = () => {
   return (
-    <Provider store={store}>
-      <PersistGate loading={<StyledLoader />} persistor={persistor}>
-        <HashRouter>
-          <TabsMenu />
-          <AppRouter />
-          <ErrorPopup />
-          <GlobalStyle />
-        </HashRouter>
-      </PersistGate>
-    </Provider>
+    <>
+      <TabsMenu />
+      <AppRouter />
+      <ErrorPopup />
+      <GlobalStyle />
+    </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,18 @@
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { HashRouter } from 'react-router-dom'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import { App } from './app/App'
+import { persistor, store } from './app/store'
+import { StyledLoader } from './common'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <Provider store={store}>
+    <PersistGate loading={<StyledLoader />} persistor={persistor}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </PersistGate>
+  </Provider>
+)
